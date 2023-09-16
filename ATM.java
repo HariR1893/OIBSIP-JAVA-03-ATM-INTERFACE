33fdc992bc84ba52016e0676fa4bf9f5acc40bd7
Hariharan Ramesh <hariramesh1893@gmail.com>
2023-09-16 17:53:49 +0530

    initial commit

--- ATM.java
+++ ATM.java
@@ -1,45 +1,86 @@
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 
-// Define a Java class named BankAccount to model a simple bank account.
-class BankAccount {
-    // Declare a private instance variable to store the account balance.
-    private double balance;
-    
-    // Declare a private instance variable to store the transaction history as a list of Transaction objects.
-    private List<Transaction> transactionHistory;
-
-    // Constructor: Initialize the account with a balance of 0.0 and an empty transaction history list.
-    public BankAccount() {
-        this.balance = 0.0;
-        this.transactionHistory = new ArrayList<>();
+public class ATM {
+    private String userName;
+    private String userPw;
+    private String enteredPin;
+    private BankAccount account;
+    Scanner scanner = new Scanner(System.in);
+    ATM() {
+        userName ="Hari";
+        userPw = "1234";
+        System.out.println("\nEnter your pin: ");
+        
+        enteredPin = scanner.next();
+        account = new BankAccount();
     }
 
-    // Method to retrieve the current balance of the account.
-    public double getBalance() {
-        return balance;
-    }
-
-    // Method to deposit money into the account, updating the balance and recording the transaction.
-    public void deposit(double amount) {
-        balance += amount;
-        transactionHistory.add(new Transaction("Credit", amount));
-    }
-
-    // Method to withdraw money from the account, if sufficient funds are available.
-    // Records the transaction as a debit if successful; otherwise, prints "Insufficient funds."
-    public void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            transactionHistory.add(new Transaction("Debit", amount));
+    public boolean userAuth() {
+        if (enteredPin.equals(userPw)) {
+            System.out.println("Welcome "+userName+"!!!");
+            return true;
         } else {
-            System.out.println("Insufficient funds.");
+            System.out.println("Incorrect password..!!");
+            return false;
         }
     }
 
-    // Method to retrieve the transaction history as a list of Transaction objects.
-    public List<Transaction> getTransactionHistory() {
-        return transactionHistory;
+    public void start() {
+        
+
+        while (true) {
+            System.out.println("\nATM Menu:");
+            System.out.println("1. Balance Inquiry");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Transaction History");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("\nBalance: Rs. " + account.getBalance());
+                    break;
+                case 2:
+                    System.out.print("\nEnter the deposit amount: Rs. ");
+                    double depositAmount = scanner.nextDouble();
+                    account.deposit(depositAmount);
+                    System.out.println("\nDeposit successful.");
+                    break;
+                case 3:
+                    System.out.print("\nEnter the withdrawal amount: Rs. ");
+                    double withdrawalAmount = scanner.nextDouble();
+                    account.withdraw(withdrawalAmount);
+                    break;
+                case 4:
+                    List<Transaction> transactions = account.getTransactionHistory();
+                    System.out.println("\nTransaction History:");
+                    for (Transaction transaction : transactions) {
+                        System.out.println(
+                                transaction.getType() + ": Rs. " + transaction.getAmount());
+                    }
+                    break;
+                case 5:
+                    System.out.println("\nExiting ATM. Have a nice day!\n\n");
+                    
+                    System.exit(0);
+                default:
+                    System.out.println("\nInvalid choice. Please try again.");
+            }
+        }
     }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        if (!atm.userAuth()) {
+            return; // Exit if authentication fails
+        }
+
+        atm.start();
+    }
+    
 }
---
+++ README.md
@@ -0,0 +1,40 @@
+# OIBSIP-JAVA-03-ATM-INTERFACE
+
+This project is an ATM interface application developed as part of the internship at Oasis Infobyte. The application allows users to perform debit and credit transactions and view their transaction history after authorization using a user PIN.
+
+## Overview
+
+The ATM Interface application is built using Java and provides a user-friendly interface for conducting ATM transactions. The application supports the following features:
+
+1. **Authorization**: Users are required to input their PIN to access the ATM functionalities.
+
+2. **Debit Transactions**: Users can make debit transactions to withdraw funds from their account.
+
+3. **Credit Transactions**: Users can make credit transactions to deposit funds into their account.
+
+4. **Transaction History**: Users can view their transaction history, including details such as transaction type, amount, and timestamp.
+
+
+
+# Usage:
+   - Input your PIN to access the ATM functionalities.
+   - Choose the transaction type (debit or credit) and enter the amount for the transaction.
+   - View transaction history to see details of previous transactions.
+
+## Getting Started
+
+To get started with the Online Examination System, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/HariR1893/OIBSIP-JAVA-03-ATM-INTERFACE.git
+   ```
+2. cd to the directory:
+   ```cd OIBSIP-JAVA-03-ATM-INTERFACE```
+
+3. Compile the java file:
+  ```javac ATM.java```
+
+4. Run the file in your local JVM:
+   ``` java ATM```
